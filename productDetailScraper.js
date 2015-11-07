5126744764df3c0287bb0416ce25167b662c8a89
Ivan Cheung <ivan@singspiel.ca>
2015-11-07 02:30:32 -0500

    - Fixed skipping functionality

--- productDetailScraper.js
+++ productDetailScraper.js
@@ -47,11 +47,7 @@ worksheetName: kWorksheetName,
 
 			//Parse data and send to Parse server
 			var itemCounter = 0;
-
-			if (itemCounter<kStartIndex)
-			{
-				resume();
-			}
+			var itemCounterProcessed = 0;
 
 			var urlPrefix = "http://www.buyma.com";
 			// var itemsFile = "CanadaItems2.txt";
@@ -62,117 +58,131 @@ worksheetName: kWorksheetName,
 			fs.writeFileSync(outFile, "[");
 			lineReader.eachLine(itemsFile, function(line, last, resume) {
 			  
-			  	var relativeURL = line;
-
-			  	//Make sure we don't process the same url twice
-			  	if (alreadyProcessedLines.indexOf(relativeURL) == -1)
-			  	{
-			  		//Track the line we already processed
-			  		alreadyProcessedLines.push(relativeURL);
-
-				  	var url = urlPrefix + relativeURL;
-				  	console.log("Processing " + itemCounter  + " at: " + url);
-					request({uri: url}, 
-						function(error, response, body) {			  	    	
-					    	console.log("	>Received item");
-
-					    	//Parse with cheerio
-					    	var $ = cheerio.load(body);
-
-					    	//Find items and save to array
-					    	var price = $("span.price_txt").text();
-					    	var priceOriginal = $("span.percent_refer").text();
-					    	var percentDiscount = $("span.percent_box").text();
-					    	var accessCount = $("span.ac_count").text();
-					    	var favCount = $("span.fav_count").text();
-					    	// var brand = $("a.ulinelink").text();
-					    	var name = $("span[itemprop='name']").text();
-					    	var season = $("#s_season > dd").text();
-					    	var brand = $("a[itemprop='brand']").text();
-					    	var location = $("#s_buying_area > dd > a").text();
-					    	var numberOfInquiries = $("#tabmenu_inqcnt").text();
-					    	var buyerName = $("#buyer_name > a").text();
-					    	var buyerLink = $("#buyer_name > a").attr("href");
-					    	// var buyerSex = $("#buyer_status > .fab-design-mg--l5").text();
-					    	// var buyerCreationDate = $("#buyer_status > .fab-design-mg--b15").text();
-					    	// var buyerNumberOfItemsSelling
-					    	// var buyerFunLevel = 			
-
-					    	var item = new Object();
-					    	item.price = price;
-					    	item.priceOriginal = priceOriginal;
-					    	item.percentDiscount = percentDiscount;
-					    	item.accessCount = accessCount;
-					    	item.favCount = favCount;
-					    	item.name = name;
-					    	item.season = season;
-					    	item.brand = brand;
-					    	item.location = location;
-					    	item.numberOfInquiries = numberOfInquiries;
-					    	item.buyerName = buyerName;
-					    	item.buyerLink = urlPrefix + buyerLink;
-					    	item.link = urlPrefix + relativeURL;
-					    	item.pageRanking = itemCounter;
-
-					    	console.log("	>Item details: " + JSON.stringify(item));
-
-							//Write to file directly to save memory
-							var lineToWrite =  JSON.stringify(item);
-
-							if (!last)
-							{
-								lineToWrite = lineToWrite  + ", ";
-							}
-							else
-							{
-								lineToWrite = lineToWrite + "]";
-							}
-
-							//Write to file
-							fs.appendFileSync(outFile, lineToWrite);
-
-							//Convert item to spreadsheet format
-							var rowItem = new Object();
-							var keys = Object.getOwnPropertyNames(item);
-							var itemAsRow = new Object();
-							for (var keyIndex = 1; keyIndex <= keys.length; keyIndex++)
-							{
-								itemAsRow[keyIndex] = item[keys[keyIndex]];
-							}
-
-							//If is the first row, write header
-							if (info.lastRow == 1)
-							{
-								var headerItem = new Object();
-								var headersAsRow = new Object();
-								for (var keyIndex = 1; keyIndex <= keys.length; keyIndex++)
-								{
-									headersAsRow[keyIndex] = keys[keyIndex];
-								}
-								headerItem[1] = headersAsRow;
-								spreadsheet.add(headerItem);
-							}
-
-							//Increment item counter
-							itemCounter++;
-
-							//Set the row
-							var row = info.lastRow + itemCounter;
-							rowItem[row] = itemAsRow;
-							// console.log("Current row: " + JSON.stringify(rowItem));
-
-							spreadsheet.add(rowItem);
-							spreadsheet.send(function(err) {
-						      if(err) throw err;
-						      	console.log("	>Updated spreadsheet!")
-						      resume();
-						    });						
-				    });
+	  			if (itemCounter<kStartIndex)
+				{
+					resume();
 				}
 				else
 				{
-					resume();
-				}			
+				  	var relativeURL = line;
+
+				  	//Make sure we don't process the same url twice
+				  	if (alreadyProcessedLines.indexOf(relativeURL) == -1)
+				  	{
+				  		//Track the line we already processed
+				  		alreadyProcessedLines.push(relativeURL);
+
+					  	var url = urlPrefix + relativeURL;
+					  	console.log("Processing " + itemCounter  + " at: " + url);
+						request({uri: url}, 
+							function(error, response, body) {
+								if (error)
+								{
+									throw error;
+								}			  	
+
+						    	console.log("	>Received item");
+
+						    	//Parse with cheerio
+						    	var $ = cheerio.load(body);
+
+						    	//Find items and save to array
+						    	var price = $("span.price_txt").text();
+						    	var priceOriginal = $("span.percent_refer").text();
+						    	var percentDiscount = $("span.percent_box").text();
+						    	var accessCount = $("span.ac_count").text();
+						    	var favCount = $("span.fav_count").text();
+						    	// var brand = $("a.ulinelink").text();
+						    	var name = $("span[itemprop='name']").text();
+						    	var season = $("#s_season > dd").text();
+						    	var brand = $("a[itemprop='brand']").text();
+						    	var location = $("#s_buying_area > dd > a").text();
+						    	var numberOfInquiries = $("#tabmenu_inqcnt").text();
+						    	var buyerName = $("#buyer_name > a").text();
+						    	var buyerLink = $("#buyer_name > a").attr("href");
+						    	// var buyerSex = $("#buyer_status > .fab-design-mg--l5").text();
+						    	// var buyerCreationDate = $("#buyer_status > .fab-design-mg--b15").text();
+						    	// var buyerNumberOfItemsSelling
+						    	// var buyerFunLevel = 			
+
+						    	var item = new Object();
+						    	item.price = price;
+						    	item.priceOriginal = priceOriginal;
+						    	item.percentDiscount = percentDiscount;
+						    	item.accessCount = accessCount;
+						    	item.favCount = favCount;
+						    	item.name = name;
+						    	item.season = season;
+						    	item.brand = brand;
+						    	item.location = location;
+						    	item.numberOfInquiries = numberOfInquiries;
+						    	item.buyerName = buyerName;
+						    	item.buyerLink = urlPrefix + buyerLink;
+						    	item.link = urlPrefix + relativeURL;
+						    	item.pageRanking = itemCounter;
+
+						    	console.log("	>Item details: " + JSON.stringify(item));
+
+								//Write to file directly to save memory
+								var lineToWrite =  JSON.stringify(item);
+
+								if (!last)
+								{
+									lineToWrite = lineToWrite  + ", ";
+								}
+								else
+								{
+									lineToWrite = lineToWrite + "]";
+								}
+
+								//Write to file
+								fs.appendFileSync(outFile, lineToWrite);
+
+								//Convert item to spreadsheet format
+								var rowItem = new Object();
+								var keys = Object.getOwnPropertyNames(item);
+								var itemAsRow = new Object();
+								for (var keyIndex = 1; keyIndex <= keys.length; keyIndex++)
+								{
+									itemAsRow[keyIndex] = item[keys[keyIndex]];
+								}
+
+								//If is the first row, write header
+								if (info.lastRow == 1)
+								{
+									var headerItem = new Object();
+									var headersAsRow = new Object();
+									for (var keyIndex = 1; keyIndex <= keys.length; keyIndex++)
+									{
+										headersAsRow[keyIndex] = keys[keyIndex];
+									}
+									headerItem[1] = headersAsRow;
+									spreadsheet.add(headerItem);
+								}
+
+								//Increment item counter
+								itemCounterProcessed++;
+
+								//Set the row
+								var row = info.lastRow + itemCounterProcessed;
+								rowItem[row] = itemAsRow;
+								// console.log("Current row: " + JSON.stringify(rowItem));
+
+								spreadsheet.add(rowItem);
+								spreadsheet.send(function(err) {
+							      if(err) throw err;
+							      	console.log("	>Updated spreadsheet!")
+							      resume();
+							    });						
+					    });
+					}
+					else
+					{
+						resume();
+					}		
+				}	
+
+				itemCounter++;
 		    });
 		});
 	  });
