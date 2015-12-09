9b023f97e85ce428992630bbf17481c98df34b90
Ivan Cheung <ivan@singspiel.ca>
2015-12-09 01:10:26 -0500

    - buymaSubmitter: Brought it to new heights, including currency conversion for price

--- buymaSubmitter/buymaSubmitter.js
+++ buymaSubmitter/buymaSubmitter.js
@@ -6,21 +6,19 @@ var cheerio = require("cheerio")
 var fs = require("fs");
 var util = require('util');
 var Parse = require("parse/node");
+var fx = require('money');
+var oxr = require('open-exchange-rates');
+oxr.set({ app_id: 'dfd2acfdf4d64bfea3fe0f9fa673485a' })
+
 Parse.initialize("ZjuQjiF1XPzUA2iY82cGKhmE5czhi4AhCnbNNeTO", "ulaJU0JGr7oPGXB9nolxMzUvXn2gV1otu8MTOVJw", "cD3d8LxY8CnS180zczxQGjjMKycE2la8UNPwnmYJ");
 
 var kEmail = 'ivyangel2015@gmail.com';
 var kPassword = 'canada2015!'
 
-var url = "http://www.buyma.com/my/itemedit/";//http://shop.lululemon.com/products/clothes-accessories/relaxed-sensation/Jet-Crop-Slim-Luxtreme?c_4";
+var kProductEditUrl = "http://www.buyma.com/my/itemedit/";//http://shop.lululemon.com/products/clothes-accessories/relaxed-sensation/Jet-Crop-Slim-Luxtreme?c_4";
 var kTimeoutInMilliseconds = 10000;
+
 //Selenium
-// var webdriver = require('selenium-webdriver'),
-//     By = webdriver.By,
-//     Key = webdriver.Key,
-//     until = webdriver.until;
-// var driver = new webdriver.Builder()
-//     .forBrowser('firefox')
-//     .build();	
 var webdriverio = require('webdriverio');
 var options = {
     desiredCapabilities: {
@@ -30,184 +28,166 @@ var options = {
 
 //Product
 var kTempFilename = 'temp.png';
-var objectId = "ZfX011hDbA";
+var objectId = "jkuBiAlXGd";
 
 //Get the page
 console.log("1. Logging in");
 var client = webdriverio
     .remote(options)
     .init()
-    .url(url)
+    .url(kProductEditUrl)
     .timeoutsImplicitWait(10000)
-    // .waitForVisible('.dummyWait', 10000)
-    // .setValue('#q', 'webdriver')
     .setValue('#txtLoginId', kEmail)
     .setValue('#txtLoginPass', kPassword)
     .click('#login_do')
-    .click('.js-new-release-tour__close-bt')
-    .click('.js-release-alert__close-bt')
+    .click('.js-new-release-tour__close-bt')	//Close annoying popup
+    .click('.js-release-alert__close-bt')		//Close annoying popup
 
-// driver.get(url);
-// driver.findElement(By.id('txtLoginId')).sendKeys(kEmail);
-// driver.findElement(By.id('txtLoginPass')).sendKeys(kPassword);
-// driver.findElement(By.id('login_do')).click();
+var Product = Parse.Object.extend("Product");
+var query = new Parse.Query(Product);
+query.equalTo("objectId", objectId);
+console.log("2. Looking for an existing product");
+query.first().then(function(productFromParse)
+{
+	if (productFromParse != null)
+	{
+		console.log(">Found the product");
 
-// // //Close annoying popup
-// driver.findElement(By.className('js-new-release-tour__close-bt')).click();
-// driver.findElement(By.className('js-release-alert__close-bt')).click().then(
-// 	function()
-// 	{		
-		var Product = Parse.Object.extend("Product");
-		var query = new Parse.Query(Product);
-		query.equalTo("objectId", objectId);
-		console.log("2. Looking for an existing product");
-		query.first().then(function(productFromParse)
-		{
-			if (productFromParse != null)
-			{
-				console.log("	>Found the product");
+		var product = productFromParse.toJSON();
+		// console.log(util.inspect(product));
+		//Product edit page
+		return processSwatchRecursive(product, 0)
+	}
+	else
+	{
+		console.log("	>Could not find product");
+	}
+}).then(
+	function()
+	{
+		console.log("Finished all swatches for product!");
+	}
+)
 
-				var product = productFromParse.toJSON();
-				var swatchId = 0
-// 				// for (var swatchId = 0; swatchId < product.swatches.length; swatchId++)
-// 				{
-					var swatch = product.swatches[swatchId];
-
-					//Product edit page
-					//Category
-					client.click('.popup_category').then(
-						function()
-						{
-							// var categoryElements = [product.categoryPrimaryBuyma, product.categorySecondaryBuyma];
-							return client.click("a[value='"+ product.categoryPrimaryBuyma + "']")
-							.click("a[value='"+ product.categorySecondaryBuyma + "']");
-							// categoryElements.forEach(
-							// 	function(categoryElement)
-							// 	{
-							// 		// driver.wait(until.elementLocated(By.css("a[value='"+ categoryElement + "']")), kTimeoutInSeconds * 1000)
-							// 			// .then(function(element) {
-							// 				console.log('	>Category finished!');
-							// 			    client.click("a[value='"+ categoryElement + "']");
-							// 			// });
-							// 	});
-						}).then(
-							function()
-							{
-								console.log('	>Category finished!');
-								return client.setValue('input[name=\'itemedit[syo_name]\']', product.nameBuyma);
-							}
-						).then(
-							function()
-							{
-								return loadBrand(client, product.brand);
-							}
-
-						).then(
-							function()
-							{
-								return loadSizeAndColor(client, swatch, product.sizeCountryCode);
-							}
-						).then(
-							function()
-							{
-								var directoryName = path.dirname(require.main.filename);
-								var filePath = directoryName + "/" + kTempFilename;
-								return loadImageRecursive(client, 0, swatch.images, filePath);
-							}
-						).then(
-							function()
-							{
-								console.log('	>Finished!');
-							}
-						).catch(
+function processSwatchRecursive(product, swatchIndex)
+{
+	// return new Promise(
+	// 	function (resolve, reject) {
+		console.log('>Processing swatch at index: ' + swatchIndex);
+		var swatch = product.swatches[swatchIndex];		
+		//Category
+		return client.url(kProductEditUrl)
+		.click('.popup_category')
+			.then(
+				function()
+				{
+					// try{
+						//Category
+						var primaryCategorySelector = "a[value='"+ product.categoryPrimaryBuyma + "']";
+						var secondaryCategorySelector = "a[value='"+ product.categorySecondaryBuyma + "']";
+						return client.waitForVisible(primaryCategorySelector, kTimeoutInMilliseconds)
+						.click(primaryCategorySelector)
+						.waitForVisible(secondaryCategorySelector, kTimeoutInMilliseconds)
+						.click(secondaryCategorySelector)
+						.catch(
 							function(err)
-							{
-								console.error(err);
-							});
-					
+							{									
+								console.error('	>Category could not be loaded!');
+								return Promise.resolve();
+							})
+					// }catch(e){
+					// 	console.log('	>Category not found!');
+					// 	return Promise.resolve();
+					// }						
+				}
+			).then(
+				function()
+				{
+					//Name
+					console.log('	>Category finished!');
+					return client.setValue('input[name=\'itemedit[syo_name]\']', getStylizedName(product, swatch))
+					.catch(
+							function(err)
+							{									
+								console.error('	>Name could not be loaded!');
+								return Promise.resolve();
+							})
+				}
+			).then(
+				function()
+				{
+					return loadBrand(client, product.brand);
+				}
 
-// 					//Name
-// 					driver.findElement(By.name('itemedit[syo_name]')).sendKeys(product.nameBuyma);
+			).then(
+				function()
+				{
+					return loadSizeAndColor(client, swatch, product.sizeCountryCode);
+				}
+			).then(
+				function()
+				{
+					//Images
+					var directoryName = path.dirname(require.main.filename);
+					var filePath = directoryName + "/" + kTempFilename;
+					return loadImageRecursive(client, 0, swatch.images, filePath);
+				}
+			)
+			.then(
+				function()
+				{
+					//Misc
+					var description = product.descriptionJP != null ? product.descriptionJP : '';
+					var quantity = product.quantityBuyma != null ? product.quantityBuyma : 1;
+					var shopName = product.shopNameBuyma != null ? product.shopNameBuyma : '';
+					var price = product.price != null ? getAdjustedPrice(product) : '';
+					return client.setValue('#item_comment', description)	//Comment
+					.setValue('[name=\'itemedit[yukosu]\']', quantity)	//Quantity
+					.setValue('[name=\'itemedit[konyuchi]\']', shopName)	//Shop name
+					.setValue('[name=\'itemedit[price]\']', price)	//Price, must be high enough. Too low will cause rejection
+					.click('#draftButton')
+					.click('#done')
+					.catch(
+							function(err)
+							{									
+								console.error('	>Misc could not be loaded!');
+								return Promise.resolve();
+							})
 
-					
+					//Season
+					//Tag .popup_tags
+					//Theme
+					//Shipping
+					//通常出品価格/参考価格
+					//購入者の支払方法
+					//購入期限(日本時間)
+					//買付地
+					//発送地
+				}
+			).then(
+				function()
+				{
+					console.log('	>Finished!');
 
-// 					//Add images	
-// 					var imageUrls = swatch.images;
-// 					var directoryName = path.dirname(require.main.filename);
-// 					var filePath = directoryName + "/" + kTempFilename;
-// 					driver.wait(until.elementLocated(By.className("js-async-file-upload")), kTimeoutInSeconds * 1000)
-// 						.then(function() {
-// 							loadImageRecursive(0, imageUrls, filePath).then
-// 							(
-// 								function()
-// 								{
-// 									// console.log("	>Finished loading images!");
-// 									return loadBrand(product.brand);								
-// 								}
-// 							).then(
-// 								function()
-// 								{
-// 									// console.log("	>Finished loading brand!");
-// 									// console.log("Swatch = " + util.inspect(swatch));
-// 									return loadSizeAndColor(driver, swatch, product.sizeCountryCode);
-// 								}
-// 							).then(
-// 								function()
-// 								{						
-// 									//Comment
-// 									driver.findElement(By.id('item_comment')).sendKeys(product.descriptionJP);			
-
-// 									//Pricing: CAUSES SERVER TO REJECT THE SAVE =(
-// 									// driver.findElement(By.name('itemedit[price]')).sendKeys(product.priceBuyma);
-
-// 									//Quantity
-// 									driver.findElement(By.name('itemedit[yukosu]')).sendKeys(product.quantityBuyma);
-
-// 									//Shop name
-// 									driver.findElement(By.name('itemedit[konyuchi]')).sendKeys(product.shopNameBuyma);
-
-// 									//Tags
-// 									// driver.wait(until.elementLocated(By.className("popup_tags")), kTimeoutInSeconds * 1000)
-// 									// 	.then(function(element) {
-// 									// 		element.click();
-// 									// 	});						
-
-// 									//Draft button
-// 									driver.findElement(By.id('draftButton')).click().then(
-// 										function()
-// 										{
-// 											console.log('	>Draft button clicked!')
-// 										})
-
-// 									// //Confirm button
-// 									// driver.wait(until.elementLocated(By.id("done")), kTimeoutInSeconds * 1000)
-// 									// 	.then(function(element) {
-// 									// 		// element.click();
-// 									// 	});
-
-// 									pause(driver);
-// 									// //Pause
-// 									// driver.wait(function() {
-// 									//   return driver.findElements(By.id('asdffoo')).then(function(elements) {
-// 									//     return elements[0];
-// 									//   });
-// 									// }, 100000, 'Failed to find element after timeout');
-// 								}
-// 							).catch(
-// 							function(err)
-// 							{
-// 								console.error(err);
-// 							});
-// 						});										
-// 				}
-			}
-			else
-			{
-				console.log("	>Could not find product");
-			}
-		});
-//	}
-// );
+					var nextIndex = swatchIndex+1;
+					if (nextIndex < product.swatches.length)
+					{
+						//Move onto next swatch
+						return processSwatchRecursive(product, nextIndex);
+					}
+					// else
+					// {
+					// 	resolve();
+					// }
+				}
+			).catch(
+				function(err)
+				{
+					console.error(err);
+					// resolve();
+				});
+		// });
+}
 
 function translateToBuymaCountryName(countryCode)
 {
@@ -250,17 +230,17 @@ function loadImageRecursive(client, imageIndex, imageUrls, filePath)
 			    r.on('close', function()
 					{
 						//Upload to site
-						console.log("	>Finished downloading!");
+						console.log("	>loadImageRecursive: Finished downloading!");
 						
 						var uploadName = "updfile" + (imageIndex+1);
-						console.log("	>Upload name: " + uploadName);
+						// console.log("	>loadImageRecursive Upload name: " + uploadName);
 						var uploadSelector = 'input[name=\"' + uploadName + '\"]';
 						client//.waitForVisible(uploadSelector, 50*10000)
 							.chooseFile(uploadSelector, filePath)
 									.then(
 										function()
 										{
-											console.log("		>Upload finished");
+											console.log("		>loadImageRecursive: Upload finished");
 
 											//Delete temp.png
 											fs.unlink(filePath);	
@@ -279,10 +259,15 @@ function loadImageRecursive(client, imageIndex, imageUrls, filePath)
 			}			
 			else
 			{
-				console.log("	>Images all uploaded!");
+				console.log("	>loadImageRecursive: Images all uploaded!");
 			}	
 		}
-	);
+	).catch(
+		function(err)
+		{
+			console.log("	>loadImageRecursive: Images could not be loaded!")
+		}
+	)
 }
 
 function loadSizeAndColor(client, swatch, sizeCountryCode)
@@ -317,7 +302,7 @@ function loadSizeAndColor(client, swatch, sizeCountryCode)
 	  						return getBuymaSizeName(size, sizeCountryCode);
 	  					});
 
-					console.log('	loadSizeAndColor buymaSizeNames: ' + util.inspect(buymaSizeNames));
+					console.log('	>loadSizeAndColor buymaSizeNames: ' + util.inspect(buymaSizeNames));
 					
 					client.waitForVisible('input[name=colorsize_2_5]', kTimeoutInMilliseconds)
 					.elements('tr.js-color-size-size-table').then(
@@ -341,7 +326,7 @@ function loadSizeAndColor(client, swatch, sizeCountryCode)
 											var rowValue = $('input.js-size-text').val();
 											var checkboxName = $('input.js-colorsize-checkbox').attr('name');
 											var isMatch = buymaSizeNames.indexOf(rowValue) != -1;
-											console.log('	>loadSizeAndColor value: ' + rowValue + ' checkbox-name: ' + checkboxName + ' isMatch: ' + isMatch);
+											// console.log('	>loadSizeAndColor value: ' + rowValue + ' checkbox-name: ' + checkboxName + ' isMatch: ' + isMatch);
 											if (!isMatch)
 											{
 												return client.click('input[name=' + checkboxName + ']')
@@ -361,8 +346,14 @@ function loadSizeAndColor(client, swatch, sizeCountryCode)
 								console.log('	>loadSizeAndColor: Finished');
 								resolve();
 							}
-						);
-				})
+						)
+				}).catch(
+							function(err)
+							{
+								console.error('	>loadSizeAndColor: Size and color could not be loaded!');
+								resolve();
+							}
+						)
 		})
 }
 
@@ -371,32 +362,110 @@ function loadBrand(client, brand)
 	return new Promise(
 		function(resolve, reject)
 		{
+			console.log("	>loadBrand: Started");
 			// Brand
 			// var brand = product.brand;
-			console.log("	>loadBrand: Started");
+			var firstLetter = brand.slice(0,1);
+			var firstLetterSelector = '.brand_list_popup[brandindex=' + firstLetter + ']';
+			console.log('	>loadBrand: First letter is ' + firstLetter);			
 			client.click('.popup_brand')
-			.waitForVisible('#brand_suggest_inputTxt', 10000)
-			.click('#brand_suggest_inputTxt > input')
-			.keys(brand)
-			.click('input.close_box')
-			// .waitForVisible('.bm_auto_complete', 10000)
-			// .click('.bm_auto_complete')
+			.waitForVisible(firstLetterSelector, kTimeoutInMilliseconds)
+			.click(firstLetterSelector)	//Click the first letter
+			// .waitForVisible('a*=' + 'L\'ecole', 10000)
+			// .click('a*=' + brand)							//Click the brand with the matching row
+			.elements('ul.brand_list > li > a').then(
+			function(result)
+			{
+				var elements = result.value;
+				// console.log('	>loadSizeAndColor: Found elements:' + util.inspect(elements));
+				var isAlreadyMatched = false;
+				var promises = elements.map(
+					function(element)
+					{
+						// console.log(util.inspect(element));
+						var elementId = element.ELEMENT;
+						// console.log('	>loadSizeAndColor id: ' + elementId);
+						return client
+						.elementIdAttribute(elementId, 'outerHTML')
+						.then(
+							function(result)
+							{									
+								var elementValue = result.value;	
+								// console.log(elementValue);
+								var $ = cheerio.load(elementValue);
+								var value = $('a').attr('value');
+								var textValue = $('a').text();
+								// var checkboxName = $('input.js-colorsize-checkbox').attr('name');
+
+								var isMatch = textValue.match(brand, 'i') != null;
+								// console.log('	>loadBrand value: ' + value + ' text:' + textValue + ' isMatch: ' + isMatch);
+								if (isMatch&&!isAlreadyMatched)
+								{
+									console.log('	>loadBrand: Match found!');
+									return client.click('a[value=\'' + value + '\']');
+								}
+							});								
+					});		
+				return Promise.all(promises);
+			})
 			.then(
 				function()
 				{
 					console.log('	>loadBrand: Finished!');
 					resolve();
 				}
-			);
+			).catch(
+			function(err)
+			{
+				console.error('	>loadBrand: Brand could not be loaded!');
+				return resolve();
+			})
 		});
 }
 
-// function pause(driver)
-// {
-// 	//Pause
-// 	driver.wait(function() {
-// 	  return driver.findElements(By.id('asdffoo')).then(function(elements) {
-// 	    return elements[0];
-// 	  });
-// 	}, 100000, 'Failed to find element after timeout');
-// }
+function getShippingCost(product)
+{
+	//TODO switch based on categories
+	return 40;
+}
+
+function getAdjustedPrice(product)
+{
+	// console.log('	>getAdjustedPrice: price (original)=' + product.price);
+	var salesTax = 1.13;
+	var shippingCost = getShippingCost(product);
+	var markup = 1.40;
+	// console.log('	>getAdjustedPrice: getShippingCost=' + shippingCost);
+	var adjustedPrice = (product.price * salesTax) * markup + shippingCost;
+	// console.log('	>getAdjustedPrice: adjustedPrice=' + adjustedPrice);
+
+	//Convert to Japanese yen
+	oxr.latest(function() {
+		// Apply exchange rates and base rate to `fx` library object:
+		fx.rates = oxr.rates;
+		fx.base = oxr.base;
+
+		// console.log('	>getAdjustedPrice: price (adjusted)=' + adjustedPrice);
+		var priceInJPY = fx(adjustedPrice).from("CAD").to("JPY");
+		// console.log('	>getAdjustedPrice: price (adjusted in JPY)=' + priceInJPY);
+		//Round up to nearest 900
+		priceInJPY = Math.round(priceInJPY/1000)*1000 - 100;
+		console.log('	>getAdjustedPrice: price (rounded in JPY)=' + priceInJPY);
+		return priceInJPY;
+	});
+}
+
+function getStylizedName(product, swatch)
+{
+	var productName = product.nameBuyma != null ? product.nameBuyma : product.name;
+	// var brandDescription = '';
+
+	// switch (product.brand)
+	// {
+	// 	case 'lululemon':
+	// 	brandDescription = 'ヨガ&普段使い';
+	// 	break;
+	// }	
+
+	return util.format('【%s】%s ★ %s', product.brand, productName, swatch.descriptionBuyma);
+}
